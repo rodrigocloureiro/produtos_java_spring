14b4f04c28866eb5d9f03cc9f4d4585a184d1666
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2023-11-28 18:18:14 -0300

    projeto iniciado

---
+++ src/test/java/br/com/infnet/at/AtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infnet.at;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
